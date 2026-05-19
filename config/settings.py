1b4162ec6b917dbf466ad73178e5b089267dfa1c
Igorello <103700053+Igor-ello@users.noreply.github.com>
2026-05-20 01:06:00 +0300

    Добавлены redis и nginx

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
